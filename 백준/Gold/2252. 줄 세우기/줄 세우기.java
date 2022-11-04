3ab713ce207c529b91da9bd2a2d7fd90b335c7b1
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2022-11-05 01:41:22 +1100

    [Gold III] Title: 줄 세우기, Time: 472 ms, Memory: 43900 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,51 +1,50 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     
     static int N, M;
+    static ArrayList<Integer>[] adj;
     static int[] indeg;
-    static ArrayList<Integer>[] graph;
     
-    static void input() {
-        Scanner sc = new Scanner(System.in);
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        N = sc.nextInt();
-        M = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        adj = new ArrayList[N + 1];
         indeg = new int[N + 1];
-        graph = new ArrayList[N + 1];
-        
-        for(int i = 0; i <= N; i++) {
-            graph[i] = new ArrayList<Integer>();
-        }
-        
+        for(int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
         for(int i = 0; i < M; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            graph[x].add(y);
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            adj[x].add(y);
             indeg[y]++;
         }
     }
     
     static void pro() {
-        Deque<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 1; i <= N; i++) {
             if(indeg[i] == 0) q.add(i);
         }
         while(!q.isEmpty()) {
             int x = q.poll();
             sb.append(x).append(' ');
-            for(int y : graph[x]) {
+            for(int y : adj[x]) {
                 indeg[y]--;
                 if(indeg[y] == 0) q.add(y);
             }
         }
-        
         System.out.println(sb);
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         input();
         pro();
     }
