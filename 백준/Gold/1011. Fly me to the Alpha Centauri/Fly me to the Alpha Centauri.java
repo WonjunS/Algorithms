5c6ade8517fb2f14d83e17fcbd73ad63bbfd017d
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-01-05 12:53:46 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 120 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int d = y - x;
+            int s = (int) Math.sqrt(d);
+            
+            if(s * s == d) {
+                sb.append(2 * s - 1).append('\n');
+            } else if(s * s + s >= d) {
+                sb.append(2 * s).append('\n');
+            } else {
+                sb.append(2 * s + 1).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
