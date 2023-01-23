d30f1ba5df8a40a2320aa8089747327dbe3f1397
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-01-23 22:34:57 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,14 +1,8 @@
 -- 코드를 입력하세요
-SELECT LPAD(A.rest_id, 5, 0) AS "REST_ID"
-     , A.rest_name AS "REST_NAME"
-     , A.food_type AS "FOOD_TYPE"
-     , A.favorites AS "FAVORITES"
-     , A.address   AS "ADDRESS"
-     , ROUND(AVG(B.review_score), 2) AS "SCORE"
-  FROM rest_info A
-    INNER JOIN rest_review B
-            ON (A.rest_id = B.rest_id)
- WHERE SUBSTR(A.address, 1, 2) = '서울' 
- GROUP BY LPAD(A.rest_id, 5, 0), A.rest_name, A.food_type, A.favorites, A.address
- ORDER BY score desc, favorites desc
-;
+SELECT A.REST_ID, A.REST_NAME,  A.FOOD_TYPE, A.FAVORITES,   A.ADDRESS, ROUND(AVG(NVL(B.REVIEW_SCORE, 0)), 2) SCORE
+  FROM REST_INFO  A
+       INNER JOIN REST_REVIEW B
+              ON A.REST_ID = B.REST_ID
+ WHERE A.ADDRESS LIKE '서울%'
+ GROUP BY A.REST_ID, A.REST_NAME,   A.FOOD_TYPE, A.FAVORITES,   A.ADDRESS
+ ORDER BY ROUND(AVG(NVL(B.REVIEW_SCORE, 0)), 2) DESC, A.FAVORITES DESC
