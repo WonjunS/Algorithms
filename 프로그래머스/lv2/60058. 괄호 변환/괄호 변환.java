4d13f6578fe5d6725c2cd0fc7f77fc64f1ca0a01
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-02-19 13:38:15 +0900

    [level 2] Title: 괄호 변환, Time: 2.86 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,63 @@
+import java.util.Stack;
+
+class Solution {
+    
+    public String solution(String p) {
+        String answer = dfs(p);
+        
+        return answer;
+    }
+    
+    static String dfs(String w) {
+        if(w.equals("")) {
+            return w;
+        }
+        
+        int left = 0, right = 0, i = 0;
+        for(i = 0; i < w.length(); i++) {
+            if(w.charAt(i) == '(') left++;
+            if(w.charAt(i) == ')') right++;
+            if(left == right) break;
+        }
+        
+        String u = w.substring(0, i + 1);
+        String v = w.substring(i + 1);
+        
+        if(isCorrect(u)) {
+            v = dfs(v);
+        }
+        
+        if(!isCorrect(u)) {
+            String str = "(";
+            str = str + dfs(v);
+            str = str + ")";
+            
+            for(int j = 1; j < u.length() - 1; j++) {
+                if(u.charAt(j) == '(') str = str + ")";
+                if(u.charAt(j) == ')') str = str + "(";
+            }
+            
+            return str;
+        }
+        
+        return u + v;
+    }
+    
+    static boolean isCorrect(String s) {
+        Stack<Character> stk = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == '(') stk.push(c);
+            else {
+                if(stk.isEmpty()) return false;
+                else stk.pop();
+            }
+        }
+        
+        if(stk.size() > 0) return false;
+        
+        return true;
+    }
+}
