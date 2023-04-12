a8b24bdff5ebf441a5515fdab2286ef6c9cc5a67
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-04-12 22:26:29 +0900

    [unrated] Title: 광물 캐기, Time: 2.15 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
@@ -2,97 +2,71 @@ import java.util.*;
 
 class Solution {
     
-    static int answer, length;
-    static int[][] board;
-    static List<Integer> list;
-    static Map<Integer, Integer> map;
-    
-    public int solution(int[] picks, String[] minerals) {
-        board = new int[3][3];
-        
-        // 0 -> diamond, 1 -> iron, 2 -> stone
-        board[0][0] = 1;
-        board[0][1] = 1;
-        board[0][2] = 1;
-        
-        board[1][0] = 5;
-        board[1][1] = 1;
-        board[1][2] = 1;
-        
-        board[2][0] = 25;
-        board[2][1] = 5;
-        board[2][2] = 1;
-        
-        int count = Arrays.stream(picks).sum();
-        List<List<Integer>> list = new ArrayList<>();
-        
-        for(int i = 0; i < minerals.length; i+=5) {
-            if(count == 0) break;
-            List<Integer> subList = new ArrayList<>();
-            for(int j = i; j < i + 5; j++) {
-                if(j >= minerals.length) break;
-                String mineral = minerals[j];
-                int val = mineral.equals("iron") ? 1 : mineral.equals("stone") ?
-                    2 : 0;
-                subList.add(val);
-            }
-            list.add(subList);
-            count--;
-        }
-        
-        List<Score> scoreList = new ArrayList<>();
-        for(List<Integer> l : list) {
-            int d = 0, i = 0, s = 0;
-            for(int m : l) {
-                d += board[0][m];
-                i += board[1][m];
-                s += board[2][m];
-            }
-            
-            scoreList.add(new Score(d, i, s));
-        }
-        
-        Collections.sort(scoreList, new Comparator<Score>() {
-            @Override
-            public int compare(Score s1, Score s2) {
-                if(s1.stone == s2.stone) {
-                    return s2.iron - s1.iron;
-                }
-                return s2.stone - s1.stone;
-            }
-        });
-        
-        for(Score score : scoreList) {
-            if(picks[0] > 0) {
-                answer += score.diamond;
-                picks[0] -= 1;
-                continue;
-            }
-            if(picks[1] > 0) {
-                answer += score.iron;
-                picks[1] -= 1;
-                continue;
-            }
-            if(picks[2] > 0) {
-                answer += score.stone;
-                picks[2] -= 1;
-                continue;
-            }
-        }
-        
-        return answer;
-    }
-    
-    static class Score {
+    static class Mineral {
         private int diamond;
         private int iron;
         private int stone;
         
-        public Score(int diamond, int iron, int stone) {
+        public Mineral(int diamond, int iron, int stone) {
             this.diamond = diamond;
             this.iron = iron;
             this.stone = stone;
         }
     }
     
+    static int[][] scoreBoard;
+    static List<Mineral> list;
+    
+    public int solution(int[] picks, String[] minerals) {
+        int answer = 0;
+        
+        scoreBoard = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+        
+        int totalPicks = Arrays.stream(picks).sum();
+        list = new ArrayList<>();
+        for(int i = 0; i < minerals.length; i+=5) {
+            if(totalPicks == 0) break;
+            
+            int dia = 0, iron = 0, stone = 0;
+            for(int j = i; j < i + 5; j++) {
+                if(j == minerals.length) break;
+                
+                String mineral = minerals[j];
+                int val = mineral.equals("iron") ? 1 : 
+                    mineral.equals("stone") ? 2 : 0;
+                
+                dia += scoreBoard[0][val];
+                iron += scoreBoard[1][val];
+                stone += scoreBoard[2][val];
+            }
+            
+            list.add(new Mineral(dia, iron, stone));
+            totalPicks--;
+        }
+        
+        Collections.sort(list, ((o1, o2) -> (o2.stone - o1.stone)));
+        for(Mineral m : list) {
+            int dia = m.diamond;
+            int iron = m.iron;
+            int stone = m.stone;
+            
+            if(picks[0] > 0) {
+                answer += dia;
+                picks[0]--;
+                continue;
+            }
+            if(picks[1] > 0) {
+                answer += iron;
+                picks[1]--;
+                continue;
+            }
+            if(picks[2] > 0) {
+                answer += stone;
+                picks[2]--;
+                continue;
+            }
+        }
+        
+        return answer;
+    }
 }
