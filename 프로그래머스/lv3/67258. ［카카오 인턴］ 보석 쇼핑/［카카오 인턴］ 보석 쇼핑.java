b130a2aeb1f9c585a8c57cac6e30180b2c974e07
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-04-22 23:50:40 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 66.68 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,25 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
-        Map<String, Integer> map = new HashMap<>();        
-        int size = new HashSet<>(Arrays.asList(gems)).size();
+        int left = 1, right = gems.length;
+        int length = Integer.MAX_VALUE;
         
-        int L = 0, length = Integer.MAX_VALUE;
-        for(int R = 0; R < gems.length; R++) {
-            map.put(gems[R], map.getOrDefault(gems[R], 0) + 1);
-            
-            while(map.get(gems[L]) > 1) {
-                map.put(gems[L], map.get(gems[L]) - 1);
-                L++;
+        Set<String> set = new HashSet<>();
+        for(String gem : gems) {
+            set.add(gem);
+        }
+        
+        int size = set.size();
+        
+        Map<String, Integer> map = new HashMap<>();
+        int R = 0;
+        for(int L = 0; L < gems.length; L++) {
+            while(R < gems.length && map.size() < size) {
+                map.put(gems[R], map.getOrDefault(gems[R], 0) + 1);
+                R++;
             }
             
             if(length > (R - L) && map.size() == size) {
-                length = R - L;
-                answer[0] = L + 1;
-                answer[1] = R + 1;
+                left = L + 1;
+                right = R;
+                length = (R - L);
+            }
+            
+            map.put(gems[L], map.get(gems[L]) - 1);
+            
+            if(map.get(gems[L]) == 0) {
+                map.remove(gems[L]);
             }
         }
+        
+        int[] answer = {left, right};
         
         return answer;
     }
