338ed4f6486bcb3b559c25674650a340d769751b
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-04-29 22:35:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.05 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -12,18 +12,21 @@ class Solution {
         array_x = new int[10];
         array_y = new int[10];
         
+        // X에 각 숫자가 몇 번 쓰였는지 저장하는 배열
         for(int i = 0; i < X.length(); i++) {
             int n = Character.getNumericValue(X.charAt(i));
             array_x[n]++;
         }
         
+        // Y에 각 숫자가 몇 번 쓰였는지 저장하는 배열
         for(int i = 0; i < Y.length(); i++) {
             int n = Character.getNumericValue(Y.charAt(i));
             array_y[n]++;
         }
         
+        // 가장 큰 정수를 만들어야 하므로, 9부터 내려감
         for(int i = 9; i >= 0; i--) {
-            int min = Math.min(array_x[i], array_y[i]);
+            int min = Math.min(array_x[i], array_y[i]); // 더 적게 쓰인 횟수
             for(int j = 0; j < min; j++) {
                 sb.append(i);
             }
