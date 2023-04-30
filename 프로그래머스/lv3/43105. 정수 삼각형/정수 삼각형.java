4b177665ab7502fc644d2a21f4057cdc877332a1
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-04-30 17:09:06 +0900

    [level 3] Title: 정수 삼각형, Time: 9.59 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,36 +1,27 @@
 class Solution {
+    
     public int solution(int[][] triangle) {
         int answer = 0;
-        int length = triangle.length;
-        int[][] Dy = new int[length][length];
+        int N = triangle.length;
         
-        
+        int[][] Dy = new int[N][N];
         Dy[0][0] = triangle[0][0];
-        if(triangle.length == 1) return Dy[0][0];
-        
-        Dy[1][0] = triangle[1][0] + Dy[0][0];
-        Dy[1][1] = triangle[1][1] + Dy[0][0];
-        
-        if(triangle.length == 2) return Math.max(Dy[1][0], Dy[1][1]);
-        
-        for(int i = 2; i < triangle.length; i++) {
+        for(int i = 1; i < N; i++) {
             for(int j = 0; j < triangle[i].length; j++) {
                 if(j == 0) {
-                    Dy[i][j] = triangle[i][j] + Dy[i - 1][0];
+                    Dy[i][j] = Dy[i - 1][j] + triangle[i][j];
                     continue;
                 }
                 if(j == triangle[i].length - 1) {
-                    Dy[i][j] = triangle[i][j] + Dy[i - 1][j - 1];
+                    Dy[i][j] = Dy[i - 1][j - 1] + triangle[i][j];
                     continue;
                 }
-                else {
-                    Dy[i][j] = triangle[i][j] + Math.max(Dy[i - 1][j - 1], Dy[i - 1][j]);
-                }
+                Dy[i][j] = Math.max(Dy[i - 1][j - 1], Dy[i - 1][j]) + triangle[i][j];
             }
         }
         
-        for(int i = 0; i < length; i++) {
-            answer = Math.max(answer, Dy[length - 1][i]);
+        for(int i = 0; i < N; i++) {
+            answer = Math.max(answer, Dy[N - 1][i]);
         }
         
         return answer;
