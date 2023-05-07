b14f5009642f43959a000b877985be0d8fdb528d
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-05-08 00:25:04 +0900

    [level 2] Title: 타겟 넘버, Time: 1.35 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -6,7 +6,7 @@ class Solution {
     
     public int solution(int[] numbers, int target) {
         N = numbers.length;
-        selected = new int[numbers.length];
+        selected = new int[N];
         
         dfs(0, target, numbers);
         
@@ -14,6 +14,7 @@ class Solution {
     }
     
     static void dfs(int k, int target, int[] numbers) {
+        // N개의 연산자가 선택된 경우 계산 후 dfs 종료
         if(k == N) {
             int sum = 0;
             for(int i = 0; i < N; i++) {
@@ -29,8 +30,9 @@ class Solution {
             return;
         }
         for(int i = 0; i <= 1; i++) {
+            // selected[k]의 값이 0이면 '+', 1이면 '-'를 선택
             selected[k] = i;
-            dfs(k + 1, target, numbers);
+            dfs(k + 1, target, numbers); // 인덱스 값 1 증가
             selected[k] = -1;
         }
     }
