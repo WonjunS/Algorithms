dfd36b35db16c24c00cd1e950cb96eb3dba0a7c6
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-08-30 15:27:21 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,39 +1,26 @@
 class Solution {
     
-    static int N, answer;
-    static int[] selected;
-    static char[] ops = {'+', '-'};
+    static int T, answer, N;
     
     public int solution(int[] numbers, int target) {
-        N = numbers.length;
-        selected = new int[N];
+        answer = 0;
         
-        dfs(0, target, numbers);
+        T = target;
+        N = numbers.length;
+        
+        dfs(0, 0, numbers);
         
         return answer;
     }
     
-    static void dfs(int k, int target, int[] numbers) {
-        // N개의 연산자가 선택된 경우 계산 후 dfs 종료
+    static void dfs(int sum, int k, int[] numbers) {
         if(k == N) {
-            int sum = 0;
-            for(int i = 0; i < N; i++) {
-                char op = ops[selected[i]];
-                if(op == '+') {
-                    sum += numbers[i];
-                } else {
-                    sum -= numbers[i];
-                }
+            if(sum == T) {
+                answer++;
             }
-            if(sum == target) answer++;
-            
             return;
         }
-        for(int i = 0; i <= 1; i++) {
-            // selected[k]의 값이 0이면 '+', 1이면 '-'를 선택
-            selected[k] = i;
-            dfs(k + 1, target, numbers); // 인덱스 값 1 증가
-            selected[k] = -1;
-        }
+        dfs(sum + numbers[k], k + 1, numbers);
+        dfs(sum - numbers[k], k + 1, numbers);
     }
 }
