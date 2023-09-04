10df5f8bab37b2600327a589b0e0840cbfe9b80a
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-09-04 15:13:03 +0900

    [Silver III] Title: 주유소, Time: 348 ms, Memory: 30500 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = 
+            new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] dist = new int[N];
+        int[] price = new int[N + 1];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            dist[i] = x;
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < N + 1; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            price[i] = x;
+        }
+        
+        int cost = price[1] * dist[1];
+        int k = 2;
+        while(k < N) {
+            int t = k + 1;
+            int need = price[k] * dist[k];
+            int x = need; // 이번에 다음꺼까지 넣는 거
+            int y = need; // 다음에 넣는 거
+            while(true) {
+                if(t == N) {
+                    cost += y;
+                    break;
+                }
+                if(x + (price[k] * dist[t]) >= y + (price[t] * dist[t])) {
+                    cost += y;
+                    break;
+                }
+                
+                x = x + (price[k] * dist[t]);
+                y = x;
+                t++;
+            }
+            if(k == t) {
+                k++;
+            } else {
+                k = t;
+            }
+        }
+        
+        System.out.println(cost);
+    }
+    
+}
