18b3853a025241522b7149c6e542aa4a43f740a3
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-09-20 20:25:43 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.39 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -1,22 +1,21 @@
 class Solution {
     
+    private static final int MOD = 1000000007;
+    
     public int solution(int n) {
-        int[] Dy = new int[5005];
+        int[] DP = new int[n + 1];
         
-        Dy[1] = 2;
-        Dy[2] = 3;
-        for(int i = 3; i <= 5000; i++) {
+        DP[1] = 2;
+        DP[2] = 3;
+        for(int i = 3; i <= n; i++) {
             if(i % 2 == 0) {
-                Dy[i] = Dy[i - 1] % 1000000007 + Dy[i - 2] % 1000000007;
+                DP[i] = DP[i - 1] % MOD + DP[i - 2] % MOD;
+            } else {
+                DP[i] = DP[i - 1] * 2 % MOD + DP[i - 2] % MOD;
             }
-            if(i % 2 != 0) {
-                Dy[i] = Dy[i - 1] * 2 % 1000000007 + Dy[i - 2] % 1000000007;
-            }
-            Dy[i] = Dy[i] % 1000000007;
+            DP[i] = DP[i] % MOD;
         }
         
-        int answer = Dy[n];
-        
-        return answer;
+        return DP[n];
     }
 }
