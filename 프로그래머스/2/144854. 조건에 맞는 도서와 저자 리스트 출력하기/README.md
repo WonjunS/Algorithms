0242ad6921261d0f54f189664c9794baaa5772f1
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2024-12-29 15:52:39 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/README.md
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조건에 맞는 도서와 저자 리스트 출력하기 - 144854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144854?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144854?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 1월 1일 13:53:49
+2024년 12월 29일 15:52:32
 
 ### 문제 설명
 
--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,5 +1,8 @@
-SELECT B.BOOK_ID, A.AUTHOR_NAME, DATE_FORMAT(B.PUBLISHED_DATE, '%Y-%m-%d')
-FROM BOOK B, AUTHOR A
-WHERE B.AUTHOR_ID = A.AUTHOR_ID
-AND B.CATEGORY = '경제'
+SELECT b.book_id, a.author_name, to_char(b.published_date, 'YYYY-MM-DD') AS PUBLISHED_DATE
+FROM Author a, (
+    SELECT BOOK_ID, AUTHOR_ID, PUBLISHED_DATE
+    FROM BOOK
+    WHERE CATEGORY = '경제'
+) b
+WHERE a.author_id = b.author_id
 ORDER BY 3;
