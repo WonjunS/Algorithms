4d66f8fa84e974fb1139ee099ffcfa9acc9d4a33
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2024-12-30 21:59:41 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.23 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    private static final int MOD = 1000000007;
+    
+    public int solution(int n) {
+        int[] DP = new int[n + 1];
+        DP[1] = 1;
+        DP[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            DP[i] = (DP[i - 2] + DP[i - 1]) % MOD;
+        }
+        
+        int answer = DP[n];
+        return answer;
+    }
+}
